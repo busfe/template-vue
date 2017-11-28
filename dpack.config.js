42be881819dfec5c0cbf9fe0ea18987d69df531e
wangyunwon <wangyunwon@didichuxing.com>
2017-11-28 16:25:02 +0800

    dpack config -> dpack.set

--- dpack.config.js
+++ dpack.config.js
@@ -2,37 +2,30 @@
 const path = require('path')
 const vueLoaderConfig = require('./build/vue-loader.conf')
 
-module.exports = function (dpackCfg) {
-  dpackCfg.entry = {
-    app: './src/main.js'
-  };
+module.exports = function (dpack, args) {
 
-  //https://github.com/vuejs-templates/webpack/issues/215
-  // to avoid vue warning: You are using the runtime-only build of Vue where the template option is not available. Either pre-compile the templates into render functions, or use the compiler-included build.
-  dpackCfg.resolve = dpackCfg.resolve || {};
-  dpackCfg.resolve.alias = dpackCfg.resolve.alias || {};
-  dpackCfg.resolve.alias.vue$ = 'vue/dist/vue.esm.js';
-
-
-  dpackCfg.resolveLoader.extensions.push('.vue');
-
-  dpackCfg.module.rules = dpackCfg.module.rules || {};
-
-  dpackCfg.module.rules.lint = {
-    test: /\.(js|vue)$/,
-    loader: 'eslint-loader',
-    enforce: 'pre',
-    include: [path.resolve('src')],
-    options: {
-      formatter: require('eslint-friendly-formatter')
+  dpack.set({
+    'entry': './src/main.js',
+    //https://github.com/vuejs-templates/webpack/issues/215
+    // to avoid vue warning: You are using the runtime-only build of Vue where the template option is not available. Either pre-compile the templates into render functions, or use the compiler-included build.
+    'resolve.alias': {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+    'module.rules.vue': {
+      test: /\.vue$/,
+      loader: 'vue-loader',
+      options: vueLoaderConfig
+    },
+    'module.rules.lint': {
+      test: /\.(js|vue)$/,
+      loader: 'eslint-loader',
+      enforce: 'pre',
+      include: [path.resolve('src')],
+      options: {
+        formatter: require('eslint-friendly-formatter')
+      }
     }
-  };
-
-  dpackCfg.module.rules.vue = {
-    test: /\.vue$/,
-    loader: 'vue-loader',
-    options: vueLoaderConfig
-  };
+  })
 
   return dpackCfg;
 }
